b5830fe9a9af7889509174c84a1aaa604835f7cf
DCRevResLabs <dc@revolutionresearch.com.au>
2020-06-28 08:54:18 +1000

    fixed portfolio columns

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -10,24 +10,29 @@ import {
   IonCardHeader,
   IonCardContent,
   IonContent,
+  IonCol,
 } from "@ionic/react";
 
 type Item = {
   src: string;
   text: string;
+  link: string;
 };
 const items: Item[] = [
   {
     src: "../images/RRL_SS_1200x800.jpg",
     text: "Revolution Research Laboratories",
+    link: "https://www.revolutionresearch.com.au/",
   },
   {
     src: "../images/VMTBCS_SS_1200x800.jpg",
     text: "Victorian Mountain Bike Championship Series",
+    link: "https://vicchampseries.com.au/",
   },
   {
     src: "../images/WeCurate_SS_1200x800.jpg",
     text: "we curate",
+    link: "https://www.wecurate.com.au/",
   },
 ];
 
@@ -35,23 +40,23 @@ const PortfolioItem: React.FC = () => (
   <IonContent>
     <IonCard>
       <IonList>
-        {items.map((image, i) => (
-          <IonItem key={i}>
-            <IonRow>
-              {/* <IonCol size="8" offset="2"> */}
-              <IonCard>
-                <IonCardHeader>{image.text}</IonCardHeader>
-                <IonThumbnail slot="start"></IonThumbnail>
-                <IonImg src={image.src} />
-                <IonCardContent>
-                  lorem ipsum dolor sit amet, consectetur adipiscing
-                </IonCardContent>
-              </IonCard>
-              {/* </IonCol> */}
+        <IonRow>
+          {items.map((image, i) => (
+            <IonCol size="6">
+              <IonItem href={image.link} target="_blank" key={i}>
+                <IonCard>
+                  <IonCardHeader>{image.text}</IonCardHeader>
+                  <IonThumbnail slot="start"></IonThumbnail>
+                  <IonImg src={image.src} />
+                  <IonCardContent>
+                    lorem ipsum dolor sit amet, consectetur adipiscing
+                  </IonCardContent>
+                </IonCard>
+              </IonItem>
               <IonItemDivider />
-            </IonRow>
-          </IonItem>
-        ))}
+            </IonCol>
+          ))}
+        </IonRow>
       </IonList>
     </IonCard>
   </IonContent>
