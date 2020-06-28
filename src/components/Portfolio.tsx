2135e9895ccc4684284bb6265fa171e614116a56
DCRevResLabs <dc@revolutionresearch.com.au>
2020-06-28 10:14:33 +1000

    rebuild

--- src/components/Portfolio.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import PortfolioItem from "./PortfolioItem";
-import "./Portfolio.css";
-
-const Portfolio: React.FC = () => <PortfolioItem />;
-
-export default Portfolio;
